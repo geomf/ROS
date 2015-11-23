6048a9f64f65519b90f0acd3363929c49ae13625
GER\lwasilew <lukasz.wasilewski@intel.com>
2015-11-23 11:09:46 +0100

    add new column to feeder table

--- db/migrate/20151118132749_create_feeders.rb
+++ db/migrate/20151118132749_create_feeders.rb
@@ -2,6 +2,8 @@ class CreateFeeders < ActiveRecord::Migration
   def change
     create_table :feeders do |t|
       t.text :name
+      t.int :lat
+      t.int :lon
       t.text :config, array: true
     end
   end
