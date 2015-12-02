9139f113403557ebda148718209b8af3dc929bf1
GER\lwasilew <lukasz.wasilewski@intel.com>
2015-12-02 09:56:42 +0100

    Change ROS to depend on Env variables provided by UPS #96873

--- config/application.rb
+++ config/application.rb
@@ -4,13 +4,31 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'active_record/connection_adapters/postgis_adapter'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ROS
   class Application < Rails::Application
+    initializer "active_record.initialize_database.override" do |app|
+
+      ActiveSupport.on_load(:active_record) do
+        if url = ENV['DATABASE_URL']
+          ActiveRecord::Base.connection_pool.disconnect!
+          parsed_url = URI.parse(url)
+          config =  {
+              adapter:             'postgis',
+              host:                parsed_url.host,
+              encoding:            'unicode',
+              database:            parsed_url.path.split("/")[-1],
+              port:                parsed_url.port,
+              username:            parsed_url.user,
+              password:            parsed_url.password
+          }
+          establish_connection(config)
+        end
+      end
+    end
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
 
